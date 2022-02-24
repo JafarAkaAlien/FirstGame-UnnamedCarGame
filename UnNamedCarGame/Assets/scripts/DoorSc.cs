fa8ffc23bd1aa2cfc24b0296c3a54eb5fc524b8f
JafarAkaAlien <cefertarverdiyevv@gmail.com>
2022-02-23 17:46:49 -0800

    NewThings

--- UnNamedCarGame/Assets/scripts/DoorSc.cs
+++ UnNamedCarGame/Assets/scripts/DoorSc.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class DoorSc : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+   public GameObject door1;
+   public GameObject door2;
+    bool toggle;
+    bool iscolesed = true;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("v") && !toggle && iscolesed)
+        {
+            toggle = true;
+        }
+        else if(Input.GetKeyDown("v") && toggle && iscolesed)
+        {
+            toggle = false;
+        }
         
+        if (door1.transform.position.y >= 69 || door1.transform.position.y<=24)
+        {
+            iscolesed = true;
+        }
+        else
+        {
+            iscolesed = false;
+        }
+        if (door1.transform.position.y < 70 && toggle)
+        {
+            door1.transform.position = door1.transform.position + 6*Time.deltaTime*new Vector3(0,1,0);
+            door2.transform.position = door2.transform.position - 6 * Time.deltaTime * new Vector3(0, 1, 0);
+        }
+        
+
+        else if(door1.transform.position.y>22 && !toggle)
+        {
+            door1.transform.position = door1.transform.position - 6 * Time.deltaTime * new Vector3(0, 1, 0);
+            door2.transform.position = door2.transform.position + 6 * Time.deltaTime * new Vector3(0, 1, 0);
+        }
+       
     }
 }
